8fd4248a2f453eed4b859c948543507a0fdfdf81
Dewa <108676698+dwmd12@users.noreply.github.com>
2023-06-23 20:44:39 +0700

    update turn into function

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-let word = "krisna adila putra"
-let increment = parseInt(word.length/2)
-let index = []
+function mockString(wordParam){
 
 
-String.prototype.replaceAt = function(index, replacement) {
-    if (index >= this.length) {
-        return this.valueOf();
+    let word = wordParam
+    let increment = parseInt(word.length/2)
+    let index = []
+
+
+    String.prototype.replaceAt = function(index, replacement) {
+        if (index >= this.length) {
+            return this.valueOf();
+        }
+        return this.substring(0, index) + replacement + this.substring(index + 1);
     }
-    return this.substring(0, index) + replacement + this.substring(index + 1);
-}
- 
-let randomNum = Math.floor((Math.random() * word.length) + 1)
+    
+    let randomNum = Math.floor((Math.random() * word.length) + 1)
 
-index.push(randomNum)
+    index.push(randomNum)
 
-for(let i = 0; i<increment-1; i++){
+    for(let i = 0; i<increment-1; i++){
 
-    let newRand= 0
+        let newRand= 0
 
-    newRand = Math.floor(Math.random() * word.length);
-    if (newRand >= randomNum && newRand+2 <= word.length) {
-        newRand += 2;
+        newRand = Math.floor(Math.random() * word.length);
+        if (newRand >= randomNum && newRand+2 <= word.length) {
+            newRand += 2;
+        }
+
+        
+        index.push(newRand)
     }
 
-    
-    index.push(newRand)
+    index.forEach(element => {
+        if(isUpperCase(word)==false){
+            word = word.replaceAt(element, word.charAt(element).toUpperCase())
+        }
+        word = word.replaceAt(element, word.charAt(element).toLowerCase())
+        
+    });
+
+    return word
+
 }
 
-index.forEach(element => {
-    word = word.replaceAt(element, word.charAt(element).toUpperCase())
-    
-});
+function isUpperCase(string) {
+    return string.toUpperCase() === string;
+}
 
-console.log(word)
-
-
-
+module.exports = mockString
